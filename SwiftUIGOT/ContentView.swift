8509eb269c78829c5bdb8cd1352de5eff1dd2418
Run <runzhu@dreampix.cn>
2024-03-15 21:25:57 +0800

    create SwiftUIGOT project

---
+++ SwiftUIGOT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIGOT
+//
+//  Created by Run on 2024/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGOT/SwiftUIGOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGOTApp.swift
+//  SwiftUIGOT
+//
+//  Created by Run on 2024/3/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
